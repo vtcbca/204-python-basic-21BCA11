1283237383d40087e8dd40a7baa4a3a86e88d32d
Divyang Chaudhari <96609123+21BCA11@users.noreply.github.com>
2022-03-12 21:25:33 +0530

    Delete Solution directory

--- Solution/Prog1.py
+++
@@ -1,8 +0,0 @@
-#Write a python script to enter 2 number and print the sum of it.
-i = int(input("Enter any Number: "))
-sum = 0
-a = i
-while (i != 0):
-    sum = sum+(i%10)
-    i = i//10
-print(a,"Sum is ",sum)
--- Solution/Prog14.py
+++
@@ -1,3 +0,0 @@
-i = 3
-for a in range(1,i+1):
-    print(" "*(i-a)+"* "*a)
